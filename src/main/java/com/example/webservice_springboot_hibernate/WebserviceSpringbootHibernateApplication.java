442fef0a06815981038d2bca4f61642c1d8969be
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2024-03-04 20:29:46 -0300

    feat: add project initial structure

---
+++ src/main/java/com/example/webservice_springboot_hibernate/WebserviceSpringbootHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.example.webservice_springboot_hibernate;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class WebserviceSpringbootHibernateApplication {
+
+	public static void main(String[] args) {
+		run(WebserviceSpringbootHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webservice_springboot_hibernate/WebserviceSpringbootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservice_springboot_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSpringbootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
